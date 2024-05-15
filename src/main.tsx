ba46f468ef9a858f9cab20ee4df550e87c414eb8
SantiCombina <santicombina@gmail.com>
2024-05-15 02:12:44 -0300

    Install react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,15 @@ import "./assets/fontawesome/css/all.min.css";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
+import "antd/dist/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>,
 );
