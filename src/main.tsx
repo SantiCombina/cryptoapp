e149b90d1b0519c0651edf9a1fb3bf4be52029d6
SantiCombina <santicombina@gmail.com>
2024-05-23 13:45:04 -0300

    Update Tailwind CSS configuration for components.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./assets/fontawesome/css/regular.min.css";
 import "./assets/fontawesome/css/solid.min.css";
 import "./assets/fontawesome/css/light.min.css";
 import "./assets/fontawesome/css/all.min.css";
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
