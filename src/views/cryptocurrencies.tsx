9237d1c0dcf31cef068156403ed1e0dcf4d6908b
SantiCombina <santicombina@gmail.com>
2024-05-24 11:35:57 -0300

    Improve code structure and readability in cryptocurrencies view

--- src/views/cryptocurrencies.tsx
+++ src/views/cryptocurrencies.tsx
@@ -13,43 +13,47 @@ interface CryptocurrenciesProps {
 
 export function Cryptocurrencies({simplified}: CryptocurrenciesProps) {
     const count = simplified ? 10 : 100;
-    const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
+    const {data: cryptosList, isLoading} = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState<CryptoCoin[] | undefined>(undefined);
-    const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
+    const searchCrypto = (query: string) => {
         const filteredData = cryptosList?.data?.coins.filter((coin) =>
-            coin.name.toLowerCase().includes(searchTerm.toLowerCase()),
+            coin.name.toLowerCase().includes(query.toLowerCase()),
         );
 
         setCryptos(filteredData);
-    }, [cryptosList, searchTerm]);
+    };
 
-    if (isFetching) return "Loading...";
-    if (!cryptos) return "No data available";
+    useEffect(() => {
+        setCryptos(cryptosList?.data?.coins);
+    }, [cryptosList]);
+
+    if (isLoading || cryptos === undefined) return "Loading...";
 
     return (
         <>
             {!simplified && (
                 <div className="sarch-crpto">
-                    <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+                    <Input placeholder="Search Cryptocurrency" onChange={(e) => searchCrypto(e.target.value)} />
                 </div>
             )}
             <Row className="crypto-card-container" gutter={[32, 32]}>
-                {cryptos?.map((currency) => (
-                    <Col key={currency.uuid} className="crypto-card" lg={16} sm={12} xs={24}>
-                        <Link to={`/crypto/${currency.uuid}`}>
-                            <Card
-                                extra={<img className="crypto-image" src={currency.iconUrl} />}
-                                title={`${currency.rank}. ${currency.name}`}
-                            >
-                                <p>Price: {millify(Number(currency.price))}</p>
-                                <p>Market Cap: {millify(Number(currency.marketCap))}</p>
-                                <p>Daily Change: {millify(Number(currency.change))}%</p>
-                            </Card>
-                        </Link>
-                    </Col>
-                ))}
+                {cryptos.length > 0
+                    ? cryptos.map((currency) => (
+                          <Col key={currency.uuid} className="crypto-card" lg={16} sm={12} xs={24}>
+                              <Link to={`/crypto/${currency.uuid}`}>
+                                  <Card
+                                      extra={<img className="crypto-image" src={currency.iconUrl} />}
+                                      title={`${currency.rank}. ${currency.name}`}
+                                  >
+                                      <p>Price: {millify(Number(currency.price))}</p>
+                                      <p>Market Cap: {millify(Number(currency.marketCap))}</p>
+                                      <p>Daily Change: {millify(Number(currency.change))}%</p>
+                                  </Card>
+                              </Link>
+                          </Col>
+                      ))
+                    : "No data available"}
             </Row>
         </>
     );
